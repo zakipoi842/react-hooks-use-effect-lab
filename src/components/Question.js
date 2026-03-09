95585a0cf2957e126b0da22f8e2c281ae87db938
zakaria <zakariyeali351@gmail.com>
2026-03-09 13:05:36 +0300

    react-hooks-use-effect-lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      onAnswered(false);
+      return;
+    }
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    const timer = setTimeout(() => {
+      setTimeRemaining((prev) => prev - 1);
+    }, 1000);
 
-  const { id, prompt, answers, correctIndex } = question;
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining, onAnswered]);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h2>{question.prompt}</h2>
+      <p>{timeRemaining} seconds remaining</p>
+
+      {question.answers.map((answer) => (
+        <button key={answer}>{answer}</button>
+      ))}
+    </div>
   );
 }
 
